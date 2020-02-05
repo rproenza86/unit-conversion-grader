260d9bf9756073a14e57864c21c7a8f6d424dfd3
Raúl Rodríguez Proenza <raul.r.proenza@gmail.com>
2020-02-05 18:58:10 -0500

    feat: Create grader component
    - Tab component with two sections
    - Unit test companion suits
    - Styles updates
    - Code clean up

--- src/components/main/App.js
+++ src/components/main/App.js
@@ -3,9 +3,7 @@ import React from 'react';
 // component dependencies
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
-
-// assets
-import logo from './logo.svg';
+import Grader from '../grader/Grader';
 
 // styles
 import './App.css';
@@ -15,16 +13,7 @@ function App() {
         <div className="App">
             <Header />
             <main className="App-main">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>Code change to test CI/CD workflow</p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                <Grader />
             </main>
             <Footer />
         </div>
--- src/components/main/tests/App.test.js
+++ src/components/main/tests/App.test.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
+
 import App from '../App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-test('renders the new text', () => {
-    const { getByText } = render(<App />);
-    const newText = getByText(/Code change/i);
-    expect(newText).toBeInTheDocument();
+describe('Main App component', () => {
+    it('should renders without crashing', () => {
+        shallow(<App />);
+    });
+    it('should display render Grader component', () => {
+        const component = shallow(<App />);
+        const tree = component.getElement();
+        expect(tree).toMatchSnapshot();
+    });
 });
